0cac4fe92b878926aa1152c3b508fac4c2299376
Lacedaimon95 <svetozar95@gmail.com>
2022-10-20 01:41:12 +0200

    master/ navbar style

--- rick-and-morty/src/components/Navbar.tsx
+++ rick-and-morty/src/components/Navbar.tsx
@@ -6,10 +6,6 @@ import { Link } from 'react-router-dom';
 export const Navbar: React.FC = () => {
     const[active,setActive] = useState<boolean>(false);
 
-    const handleActive=()=>{
-        setActive(!active);
-    }
-
     return (
         <nav className='navbar'>
             <ul className='navbar__regular-menu'>
@@ -19,11 +15,14 @@ export const Navbar: React.FC = () => {
                 <li className='navbar__regular-menu__item'><Link to={'/Locations'} className='navbar__regular-menu__item__link'>Locations</Link></li>
             </ul>
 
-            <div className={!active?"navbar__hamburger activeHamburger" : "navbar__hamburger"} onClick={()=>handleActive()}>
+            <div className={active? "navbar__hamburger activeHamburger" : "navbar__hamburger"} onClick={()=>setActive(!active)}>
                 <div className='navbar__hamburger__line one'></div>
                 <div className='navbar__hamburger__line two'></div>
                 <div className='navbar__hamburger__line three'></div>
             </div>
+           
+            <div className='navbar__overlay' style={active? {display:"block"}:{display:"none"}} onClick={()=>setActive(false)}></div>
+            
             <ul className={active?"navbar__responsive-menu activeMenu" : "navbar__responsive-menu"}>
                 <li className='navbar__responsive-menu__item'><Link to={'/'} className='navbar__responsive-menu__item__link'>Home</Link></li>
                 <li className='navbar__responsive-menu__item'><Link to={'/Characters'} className='navbar__responsive-menu__item__link'>Characters</Link></li>
