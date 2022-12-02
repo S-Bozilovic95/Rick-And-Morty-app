f5e9c83a8623ff96361bcc0a43565feb325ead64
Lacedaimon95 <svetozar95@gmail.com>
2022-12-02 16:55:28 +0100

    master/ character

--- rick-and-morty/src/App.tsx
+++ rick-and-morty/src/App.tsx
@@ -1,36 +1,43 @@
-import './App.scss';
-import { QueryClientProvider, QueryClient } from 'react-query';
-import {ReactQueryDevtools} from 'react-query/devtools';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import { Home } from './components/Home';
-import { Navbar } from './components/Navbar';
-import { Locations } from './components/locations/Locations';
-import { CharacterDetails } from './components/characters/CharacterDetails';
-import { Characters } from './components/characters/Characters';
-import { Episodes } from './components/episodes/Episodes';
-import { ContextRmProvider } from './context/Context';
+import "./App.scss";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Home } from "./components/Home";
+import { Navbar } from "./components/Navbar";
+import { Locations } from "./components/locations/Locations";
+import { CharacterDetails } from "./components/characters/CharacterDetails";
+import { Characters } from "./components/characters/Characters";
+import { Episodes } from "./components/episodes/Episodes";
+import { ContextRmProvider } from "./context/Context";
+import videoBg from "../src/assets/videos/videoBg.mp4";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
-   <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={queryClient}>
       <ContextRmProvider>
+        <video className="video-background" autoPlay loop muted>
+          <source src={videoBg} type="video/mp4" />
+        </video>
         <div className="App">
           <Router>
-            <Navbar/>
+            <Navbar />
             <Routes>
-              <Route path='/' element={<Home/>}/>
-              <Route path='/Characters' element={<Characters/>}/>
-              <Route path='/Episodes' element={<Episodes/>}/>
-              <Route path='/Locations' element={<Locations/>}/>
-              <Route path='/CharacterDetails/:charID' element={<CharacterDetails/>}/>
+              <Route path="/" element={<Home />} />
+              <Route path="/Characters" element={<Characters />} />
+              <Route path="/Episodes" element={<Episodes />} />
+              <Route path="/Locations" element={<Locations />} />
+              <Route
+                path="/CharacterDetails/:charID"
+                element={<CharacterDetails />}
+              />
             </Routes>
           </Router>
         </div>
       </ContextRmProvider>
-      <ReactQueryDevtools initialIsOpen={false} position='bottom-right'/>
-   </QueryClientProvider>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+    </QueryClientProvider>
   );
 }
 
--- rick-and-morty/src/components/Home.tsx
+++ rick-and-morty/src/components/Home.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const Home: React.FC = () => {
-
-
-    return (
-        <section className='home container'>
-            <div className='home__logo'></div>
-            <p className='home__desc'>Welcome to the <span>Rick and Morty</span> web page! <br/> Everything you need to know about your favorite show. Search for <Link to={'/Characters'} >Characters</Link>, <Link to={'/Episodes'} >Episodes</Link> or <Link to={'/Locations'} >Locations</Link> and find every detail about <br/> <span>Rick Sanchez</span>, <span>Morty Smith</span> and their adventures.</p>
-        </section>
-    );
-}
+  return (
+    <section className="home container">
+      <div className="home__logo"></div>
+      <p className="home__desc">
+        Welcome to the <span>Rick and Morty</span> web page! <br /> Everything
+        you need to know about your favorite show. Search for{" "}
+        <Link to={"/Characters"}>Characters</Link>,{" "}
+        <Link to={"/Episodes"}>Episodes</Link> or{" "}
+        <Link to={"/Locations"}>Locations</Link> and find every detail about{" "}
+        <br /> <span>Rick Sanchez</span>, <span>Morty Smith</span> and their
+        adventures.
+      </p>
+    </section>
+  );
+};
