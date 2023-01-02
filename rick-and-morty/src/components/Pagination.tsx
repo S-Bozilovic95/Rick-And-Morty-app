b08363d361eaa3abb65a04a7884b5926b010c518
Lacedaimon95 <svetozar95@gmail.com>
2023-01-02 02:47:51 +0100

    feat: added pagination style

--- rick-and-morty/src/components/Pagination.tsx
+++ rick-and-morty/src/components/Pagination.tsx
@@ -2,7 +2,10 @@ import { AxiosResponse } from "axios";
 import { FC, useState, useEffect } from "react";
 import { useQuery } from "react-query";
 import { getCurrentPage } from "../api/pagination";
-import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import { EffectCoverflow } from "swiper";
 
 type PaginationProps = {
   searchType: string;
@@ -18,13 +21,16 @@ export const Pagination: FC<PaginationProps> = ({
   selectedName,
 }) => {
   const [activePage, setActivePage] = useState(1);
+  let numbers: number[] = [];
 
+  // query
   const { data: selectedPageData } = useQuery(
     ["selectedPage", activePage, selectedName],
     () => getCurrentPage(activePage, searchType, selectedName),
     { keepPreviousData: true }
   );
 
+  // functions
   const handlePagination = (value: any) => {
     console.log(selectedPageData?.data.info.pages);
 
@@ -40,15 +46,25 @@ export const Pagination: FC<PaginationProps> = ({
     }
   };
 
+  const handlePageButtons = () => {
+    for (let i = 1; i <= selectedPageData?.data.info.pages; i++) {
+      numbers.push(i);
+    }
+  };
+  handlePageButtons();
+
+  // hooks
   useEffect(() => {
     handleCharacterData(selectedPageData);
+  }, [handleCharacterData, selectedPageData]);
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [selectedPageData]);
+  useEffect(() => {
+    setActivePage(1);
+  }, [selectedName]);
 
   return (
     <div className="button-box">
-      <button
+      {/* <button
         className="button-box__navigation-btn"
         type="button"
         disabled={activePage === 1}
@@ -67,6 +83,38 @@ export const Pagination: FC<PaginationProps> = ({
       >
         <FaChevronRight />
       </button>
+      <span className="button-box__current-page">
+        {selectedPageData?.data.info.pages}
+      </span> */}
+
+      <Swiper
+        effect={"coverflow"}
+        spaceBetween={0}
+        slidesPerView={3}
+        modules={[EffectCoverflow]}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: false,
+        }}
+        className="button-box__swiper-box"
+      >
+        {numbers.map((el: any) => {
+          return (
+            <SwiperSlide key={el.id}>
+              <button
+                className="button-box__navigation-btn"
+                type="button"
+                onClick={() => handlePagination(el)}
+              >
+                {el}
+              </button>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </div>
   );
 };
--- rick-and-morty/src/components/characters/Characters.tsx
+++ rick-and-morty/src/components/characters/Characters.tsx
@@ -24,7 +24,6 @@ export const Characters: FC = () => {
 
   useEffect(() => {
     setAllChar(allCharData);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [allCharData]);
 
   return (
